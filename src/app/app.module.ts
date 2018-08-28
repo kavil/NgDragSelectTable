bfadd171c11be22f5aa2d5013e1a9e22781d2176
huangkai <huangkai@jpush.cn>
2018-08-28 10:26:29 +0800

    fix path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { DragSelectTableModule } from 'projects/drag-select-table/src';
+import { DragSelectTableModule } from '../../projects/drag-select-table/src';
 
 @NgModule({
   declarations: [
